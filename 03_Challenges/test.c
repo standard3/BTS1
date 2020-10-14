873498d8be34ecd235e605b369a710dbd6f82065
Théo <67806187+cynikkk@users.noreply.github.com>
2020-10-14 19:33:11 +0200

    Add files via upload

---
+++ 03_Challenges/test.c
@@ -0,0 +1,95 @@
+// Ecrire un programme en C capable de crypter et décrypter des messages via le chiffre de César.
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char crypter(int decalage, char message[])
+{
+    char ch;
+    int i;
+    for (i = 0; message[i] != '\0'; ++i)
+    {
+        while (message[i])
+        {
+            putchar(tolower(message[i])); // Convertir en lowercase
+            i++;
+        }
+
+        ch = message[i];
+
+        if ( ch >= 'a' && ch <= 'z')
+        {
+            ch += decalage;
+
+            if (ch > 'z')
+            {
+                ch -= 'z' + 'a' - 1;
+            }
+            message[i] = ch;
+        }
+    }
+    return *message;
+}
+
+char decrypter(int decalage, char message[])
+{
+    char ch;
+    int i;
+    for (i = 0; message[i] != '\0'; ++i)
+    {
+        while (message[i])
+        {
+            putchar(tolower(message[i])); // Convertir en lowercase
+            i++;
+        }
+
+        ch = message[i];
+
+        if ( ch >= 'a' && ch <= 'z')
+        {
+            ch -= decalage;
+
+            if (ch > 'z')
+            {
+                ch += 'z' + 'a' + 1;
+            }
+            message[i] = ch;
+        }
+    }
+    return *message;
+}
+
+int main()
+{
+    char message[] = "";
+    int decalage = 0, taille, choix;
+
+    printf("Entrer le message : ");
+    scanf("%s", message);
+
+    printf("\nEntrer le decalage : ");
+    scanf("%d", &decalage);
+
+    do
+    {
+        taille = strlen(message);
+        printf("\nEntrer le choix :\n1. Crypter\n2.Decrypter\n");
+        scanf("%d", &choix);
+
+        if (choix == 1)
+        {
+            printf("Message crypte : %s", crypter(decalage, message));
+        }
+        else if (choix == 2)
+        {
+            printf("Message decrypte : %s", decrypter(decalage, message));
+        }
+        else
+        {
+            printf("Mauvaise saisie, reesayer.\n");
+        }
+        
+    } while (1);
+    return 0;
+}
