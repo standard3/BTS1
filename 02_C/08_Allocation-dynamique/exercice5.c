f2df0e75677d9b4d702bffdafadd1327a8e78918
cynikkk <theo.abel53@gmail.com>
2021-04-09 12:48:56 +0200

    typos

--- 02_C/08_Allocation-dynamique/exercice5.c
+++ 02_C/08_Allocation-dynamique/exercice5.c
@@ -14,11 +14,11 @@ int main()
     for (int i = 0; i < ROWS; i++)
     {
         for (int j = 0; j < COLS; j++)
-        {
             printf("[%d]", matrix[i][j]);
-        }
         printf("\n");
     }
+
+    freeMatrix(matrix, ROWS);
     return 0;
 }
 
@@ -29,7 +29,7 @@ int **allocateMatrix(int rows, int cols, int value)
     {
         matrix[i] = (int*) malloc(cols * sizeof(int));
         for (int j = 0; j < cols; j++)
-            matrix[i][j] = value;
+            matrix[i][j] = value; // Affectation
     } 
         
 
