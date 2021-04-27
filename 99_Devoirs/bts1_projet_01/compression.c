d5dcb285908f7ab97f5b614de4b987c61a5187af
cynikkk <theo.abel53@gmail.com>
2021-04-27 20:09:34 +0200

    prototypage

---
+++ 99_Devoirs/bts1_projet_01/compression.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    /* processData.exe "clé1" "valeur1[,valeur1.2] "clé2" "valeur2" */
+    
+    /* TODO:
+    * Traiter les arguments d'entrée et les ajouter à une chaine arguments[]
+    * Traduire arguments[] en binaire
+    * Convertir arguments[] en RLE
+    * Créer QR code avec arguments[]
+    */
+
+
+    return 0;
+}
