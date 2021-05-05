9aa8c358b5ffb87d4ae5ea03fb27bb8a5ac5f510
cynikkk <theo.abel53@gmail.com>
2021-05-05 22:56:36 +0200

    another bug fix

--- 99_Devoirs/bts1_projet_01/compression.c
+++ 99_Devoirs/bts1_projet_01/compression.c
@@ -74,7 +74,7 @@ int main(int argc, char * argv[])
     
 
     // Conversion des mots par leurs index dans le dictionnaire 
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < (argc - 1) / 2; i++)
     {
         itoa(findIndexOf(grammarLinesCount, dictionnary, arguments[i].key), buffer, 10);
         strcpy(arguments[i].key, buffer);
@@ -206,26 +206,30 @@ int findIndexOf(int grammarLinesCount, Identifier dictionnary[], char word[])
  * Written by Lukás Chmela
  * Released under GPLv3.
 */
-char* itoa(int value, char* result, int base) {
-		// check that the base if valid
-		if (base < 2 || base > 36) { *result = '\0'; return result; }
+char* itoa(int value, char* result, int base) 
+{
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
 
-		char* ptr = result, *ptr1 = result, tmp_char;
-		int tmp_value;
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
 
-		do {
-			tmp_value = value;
-			value /= base;
-			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-		} while ( value );
+    do 
+    {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
 
-		// Apply negative sign
-		if (tmp_value < 0) *ptr++ = '-';
-		*ptr-- = '\0';
-		while(ptr1 < ptr) {
-			tmp_char = *ptr;
-			*ptr--= *ptr1;
-			*ptr1++ = tmp_char;
-		}
-		return result;
-	}
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) 
+    {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+
+    return result;
+}
