f4efafb09fa7f3014d9ff0a921a698eb2f02cd97
cynikkk <theo.abel53@gmail.com>
2021-05-05 22:53:19 +0200

    bug fixes

--- 99_Devoirs/bts1_projet_01/compression.c
+++ 99_Devoirs/bts1_projet_01/compression.c
@@ -19,6 +19,7 @@ void  putInDictionnary        (int, FILE*,        char[]);
 char* toLowerExtended         (char[]);
 int   isDigitExtended         (char[]);
 int   findIndexOf             (int, Identifier[], char[]);
+char* itoa                    (int, char*, int);
 
 /*  TODO:
 *   Implémenter les valeurs numériques ou chaînes de caractères statiques (Surface...)
@@ -197,5 +198,34 @@ int findIndexOf(int grammarLinesCount, Identifier dictionnary[], char word[])
 {
     for (int i = 0; i < grammarLinesCount; i++)
         if (strcmp(dictionnary[i].value, word) == 0)
-            return dictionnary[i].key;
+            return atoi(dictionnary[i].key);
 }
+
+/**
+ * C++ version 0.4 char* style "itoa":
+ * Written by Lukás Chmela
+ * Released under GPLv3.
+*/
+char* itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
